e44bc2ba137832942cf0d07436f95f4225d9838e
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2023-04-20 09:12:41 +0800

    Update update_gfwlist.sh

--- trunk/user/shadowsocks/scripts/update_gfwlist.sh
+++ trunk/user/shadowsocks/scripts/update_gfwlist.sh
@@ -3,8 +3,8 @@
 set -e -o pipefail
 [ "$1" != "force" ] && [ "$(nvram get ss_update_gfwlist)" != "1" ] && exit 0
 #GFWLIST_URL="$(nvram get gfwlist_url)"
-logger -st "gfwlist" "Starting update..."
-curl -k -s -o /tmp/gfwlist_list_origin.conf --connect-timeout 15 --retry 5 https://cdn.jsdelivr.net/gh/YW5vbnltb3Vz/domain-list-community@release/gfwlist.txt
+logger -st "gfwlist" "Starting update:https://github.com/YW5vbnltb3Vz/domain-list-community/blob/release/gfwlist.txt"
+curl -L -k -S -o /tmp/gfwlist_list_origin.conf --connect-timeout 15 --retry 5 https://fastly.jsdelivr.net/gh/YW5vbnltb3Vz/domain-list-community@release/gfwlist.txt
 lua /etc_ro/ss/gfwupdate.lua
 count=`awk '{print NR}' /tmp/gfwlist_list.conf|tail -n1`
 if [ $count -gt 1000 ]; then
@@ -20,8 +20,8 @@ logger -st "SS" "重启ShadowSocksR Plus+..."
 /usr/bin/shadowsocks.sh start
 fi
 else
-logger -st "gfwlist" "列表下载失败,请重试！"
+logger -st "gfwlist" "列表下载失败,请手动复制https://github.com/YW5vbnltb3Vz/domain-list-community/blob/release/gfwlist.txt文本内容到https://base64.us进行解码"
+logger -st "gfwlist" "复制解码内容替换/etc/storage/gfwlist/gfwlist_listnew.conf"
 fi
 rm -f /tmp/gfwlist_list_origin.conf
 rm -f /tmp/gfwlist_list.conf
-
