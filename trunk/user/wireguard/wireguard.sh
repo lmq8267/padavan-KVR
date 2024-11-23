9a6476f22ac1a826a8018492dd4bbffcf4d728cd
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-11-24 07:55:28 +0800

    Update wireguard.sh

--- trunk/user/wireguard/wireguard.sh
+++ trunk/user/wireguard/wireguard.sh
@@ -13,13 +13,15 @@ start_wg() {
 	
 	[ "$wg_enable" = "1" ] || exit 1
 	if [ ! -s /etc/storage/wg0.conf ]; then
-    		logger -t "WIREGUARD" "/etc/storage/wg0.conf 配置文件为空，退出运行."
+    		logger -t "【WIREGUARD】" "/etc/storage/wg0.conf 配置文件为空，退出运行."
     		exit 1
 	fi
-	logger -t "WIREGUARD" "正在启动wireguard"
+	logger -t 【WIREGUARD】" "正在启动wireguard"
 	ifconfig ${tun_name} down
 	ip link del dev ${tun_name}
+ 	logger -t 【WIREGUARD】" "创建虚拟网卡 ${tun_name}"
 	ip link add dev ${tun_name} type wireguard
+ 	logger -t 【WIREGUARD】" "使用配置文件 /etc/storage/wg0.conf"
 	wg setconf ${tun_name} /etc/storage/wg0.conf
 	[ -z "$localip" ] || ip -4 addr add dev ${tun_name} ${localip}
 	[ -z "$localip6" ] || ip -6 addr add dev ${tun_name} ${localip6}
@@ -30,6 +32,9 @@ start_wg() {
 	iptables -I FORWARD -i ${tun_name} -j ACCEPT
 	iptables -t nat -I POSTROUTING -o ${tun_name} -j MASQUERADE
 	ifconfig ${tun_name} up
+ 	if [ $? -eq 0 ]; then
+		logger -t 【WIREGUARD】" "已启动"
+   	fi
 }
 
 
@@ -39,7 +44,7 @@ stop_wg() {
 	iptables -D INPUT -i ${tun_name} -j ACCEPT 2>/dev/null
 	iptables -D FORWARD -i ${tun_name} -j ACCEPT 2>/dev/null
 	iptables -t nat -D POSTROUTING -o ${tun_name} -j MASQUERADE 2>/dev/null
-	logger -t "WIREGUARD" "正在关闭wireguard"
+	logger -t "【WIREGUARD】" "正在关闭wireguard"
 	}
 
 
