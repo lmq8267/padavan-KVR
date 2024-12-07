1763d101c9bbc4697f300d0ed8e634bef56ddf78
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-12-07 14:01:04 +0800

    Update auth.c

--- trunk/user/openssh/openssh-9.0p1/auth.c
+++ trunk/user/openssh/openssh-9.0p1/auth.c
@@ -633,7 +633,7 @@ getpwnamallow(struct ssh *ssh, const char *user)
 	aix_restoreauthdb();
 #endif
 	if (pw == NULL) {
-		logit("无效用户 %.100s from %.100s port %d",
+		logit("invalid user %.100s from %.100s port %d",
 		    user, ssh_remote_ipaddr(ssh), ssh_remote_port(ssh));
 #ifdef CUSTOM_FAILED_LOGIN
 		record_failed_login(ssh, user,
