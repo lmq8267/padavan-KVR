1f6e074cac62a181e635e6e7abe1c298bc55fbc4
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-12-07 09:47:41 +0800

    Update cmm_wpa_adhoc.c

--- trunk/proprietary/rt_wifi/rtpci/7.3.0.1/mt7915/embedded/security/cmm_wpa_adhoc.c
+++ trunk/proprietary/rt_wifi/rtpci/7.3.0.1/mt7915/embedded/security/cmm_wpa_adhoc.c
@@ -207,7 +207,7 @@ BOOLEAN Adhoc_PeerWpaMessageSanity(
 			RTMPSendWirelessEvent(pAd, IW_MIC_DIFF_EVENT_FLAG, pEntry->Addr, pEntry->wdev->wdev_idx, 0);
 
 			if (MsgType < EAPOL_GROUP_MSG_1)
-				MTWF_LOG(DBG_CAT_SEC, DBG_SUBCAT_ALL, DBG_LVL_ERROR, ("MIC Different in pairwise msg %d of 4-way handshake!\n", MsgType));
+				//MTWF_LOG(DBG_CAT_SEC, DBG_SUBCAT_ALL, DBG_LVL_ERROR, ("MIC Different in pairwise msg %d of 4-way handshake!\n", MsgType));
 			else
 				MTWF_LOG(DBG_CAT_SEC, DBG_SUBCAT_ALL, DBG_LVL_ERROR, ("MIC Different in group msg %d of 2-way handshake!\n", (MsgType - EAPOL_PAIR_MSG_4)));
 
