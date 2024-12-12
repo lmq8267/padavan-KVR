d3e9c59517daf7454de6f6f97ea05472e53427ca
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-12-12 09:28:57 +0800

    Update vnts.sh

--- trunk/user/vnts/vnts.sh
+++ trunk/user/vnts/vnts.sh
@@ -13,13 +13,47 @@ vnts_web_port="$(nvram get vnts_web_port)"
 vnts_web_user="$(nvram get vnts_web_user)"
 vnts_web_pass="$(nvram get vnts_web_pass)"
 vnts_web_wan=$(nvram get vnts_web_wan)
-[ -z "$VNTS" ] && VNTS=/etc/storage/bin/vnts && nvram set vnts_bin=$VNTS
 [ -z "$vnts_port" ] && vnts_port="29872" && nvram set vnts_port=$vnts_port
 [ -z "$vnts_web_port" ] && vnts_web_port="29870" && nvram set vnts_web_port=$vnts_web_port
 user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 github_proxys="$(nvram get github_proxy)"
 [ -z "$github_proxys" ] && github_proxys=" "
+vnts_renum=`nvram get vnts_renum`
 
+vnts_restart () {
+relock="/var/lock/vnts_restart.lock"
+if [ "$1" = "o" ] ; then
+	nvram set vnts_renum="0"
+	[ -f $relock ] && rm -f $relock
+	return 0
+fi
+if [ "$1" = "x" ] ; then
+	vnts_renum=${vnts_renum:-"0"}
+	vnts_renum=`expr $vnts_renum + 1`
+	nvram set vnts_renum="$vnts_renum"
+	if [ "$vnts_renum" -gt "3" ] ; then
+		I=19
+		echo $I > $relock
+		logger -t "【VNT服务端】" "多次尝试启动失败，等待【"`cat $relock`"分钟】后自动尝试重新启动"
+		while [ $I -gt 0 ]; do
+			I=$(($I - 1))
+			echo $I > $relock
+			sleep 60
+			[ "$(nvram get vnts_renum)" = "0" ] && break
+   			#[ "$(nvram get vnts_enable)" = "0" ] && exit 0
+			[ $I -lt 0 ] && break
+		done
+		nvram set vnts_renum="1"
+	fi
+	[ -f $relock ] && rm -f $relock
+fi
+scriptname=$(basename $0)
+if [ ! -z "$scriptname" ] ; then
+	eval $(ps -w | grep "$scriptname" | grep -v $$ | grep -v grep | awk '{print "kill "$1";";}')
+	eval $(ps -w | grep "$scriptname" | grep -v $$ | grep -v grep | awk '{print "kill -9 "$1";";}')
+fi
+start_vnts
+}
 get_tag() {
 	curltest=`which curl`
 	logger -t "【VNT服务端】" "开始获取最新版本..."
@@ -49,17 +83,27 @@ dowload_vnts() {
 	[ ! -d "$bin_path" ] && mkdir -p "$bin_path"
 	logger -t "【VNT服务端】" "开始下载 https://github.com/lmq8267/vnts/releases/download/${tag}/vnts_mipsel-unknown-linux-musl 到 $VNTS"
 	for proxy in $github_proxys ; do
-       curl -Lko "$VNTS" "${proxy}https://github.com/lmq8267/vnts/releases/download/${tag}/vnts_mipsel-unknown-linux-musl" || wget --no-check-certificate -O "$VNTS" "${proxy}https://github.com/lmq8267/vnts/releases/download/${tag}/vnts_mipsel-unknown-linux-musl"
+ 	length=$(wget  -T 5 -t 3 "${proxy}https://github.com/lmq8267/vnts/releases/download/${tag}/vnts_mipsel-unknown-linux-musl" -O /dev/null --spider --server-response 2>&1 | grep "[Cc]ontent-[Ll]ength" | grep -Eo '[0-9]+' | tail -n 1)
+ 	length=`expr $length + 512000`
+	length=`expr $length / 1048576`
+ 	vnts_size0="$(check_disk_size $VNTS)"
+ 	[ ! -z "$length" ] && logger -t "【VNT服务端】" "程序大小 ${length}M， 程序路径可用空间 ${vnts_size0}M "
+        curl -Lko "$VNTS" "${proxy}https://github.com/lmq8267/vnts/releases/download/${tag}/vnts_mipsel-unknown-linux-musl" || wget --no-check-certificate -O "$VNTS" "${proxy}https://github.com/lmq8267/vnts/releases/download/${tag}/vnts_mipsel-unknown-linux-musl"
 	if [ "$?" = 0 ] ; then
 		chmod +x $VNTS
-		logger -t "【VNT服务端】" "$VNTS 下载成功"
-		vnts_ver=$($VNTS --version | awk -F 'version:' '{print $2}' | tr -d ' ' | tr -d '\n')
-		if [ -z "$vnts_ver" ] ; then
-			nvram set vnts_ver=""
-		else
-			nvram set vnts_ver="v${vnts_ver}"
-		fi
-		break
+  		if [ $(($($VNTS -h | wc -l))) -gt 3 ] ; then
+			logger -t "【VNT服务端】" "$VNTS 下载成功"
+			vnts_ver=$($VNTS --version | awk -F 'version:' '{print $2}' | tr -d ' ' | tr -d '\n')
+			if [ -z "$vnts_ver" ] ; then
+				nvram set vnts_ver=""
+			else
+				nvram set vnts_ver="v${vnts_ver}"
+			fi
+			break
+   		else
+     			logger -t "【VNT服务端】" "下载失败，请手动下载 ${proxy}https://github.com/lmq8267/vnts/releases/download/${tag}/vnts_mipsel-unknown-linux-musl 上传到  $VNTS"
+			rm -rf $VNTS
+  		fi
 	else
 		logger -t "【VNT服务端】" "下载失败，请手动下载 ${proxy}https://github.com/lmq8267/vnts/releases/download/${tag}/vnts_mipsel-unknown-linux-musl 上传到  $VNTS"
    	fi
@@ -98,14 +142,26 @@ vnts_keep() {
 start_vnts() {
 	[ "$vnts_enable" = "1" ] || exit 1
 	logger -t "【VNT服务端】" "正在启动vnts"
+ 	if [ -z "$VNTS" ] ; then
+  		etc_size=`check_disk_size /etc/storage`
+      		if [ "$etc_size" -gt 1 ] ; then
+			VNTS=/etc/storage/bin/vnts
+   		else
+     			VNTS=/tmp/var/vnts
+		fi
+  		nvram set vnts_bin=$VNTS
+    	fi
 	get_tag
+ 	if [ -f "$VNTS" ] ; then
+		[ ! -x "$VNTS" ] && chmod +x $VNTS
+  		[[ "$($VNTS -h 2>&1 | wc -l)" -lt 3 ]] && rm -rf $VNTS
+  	fi
  	if [ ! -f "$VNTS" ] ; then
 		logger -t "【VNT服务端】" "主程序${VNTS}不存在，开始在线下载..."
   		[ ! -d /etc/storage/bin ] && mkdir -p /etc/storage/bin
   		[ -z "$tag" ] && tag="1.2.13"
   		dowload_vnts $tag
   	fi
-  	[ ! -f "$VNTS" ] && exit 1
   	chmod +x $VNTS
   	sed -Ei '/【VNT服务端】|^$/d' /tmp/script/_opt_script_check
 	killall -9 vnts >/dev/null 2>&1
@@ -179,6 +235,7 @@ EOF
 	sleep 4
 	if [ ! -z "`pidof vnts`" ] ; then
 		logger -t "【VNT服务端】" "运行成功！"
+  		vnts_restart o
 		if [ ! -z "$vnts_port" ] ; then
 			iptables -I INPUT -p tcp --dport $vnts_port -j ACCEPT
 			iptables -I INPUT -p udp --dport $vnts_port -j ACCEPT
@@ -194,7 +251,9 @@ EOF
 		vnts_keep
 		echo `date +%s` > /tmp/vnts_time
 	else
-		logger -t "【VNT服务端】" "运行失败！"
+		logger -t "【VNT服务端】" "运行失败, 注意检查${PROG}是否下载完整,10 秒后自动尝试重新启动"
+  		sleep 10
+  		vnts_restart x
 	fi
 	exit 0
 }
