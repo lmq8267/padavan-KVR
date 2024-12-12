63075d718ceccaf5936784f528c3532de9992b7b
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-12-12 14:46:34 +0800

    Update adguardhome.sh

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -81,21 +81,60 @@ EEE
   chmod 755 "$adg_file"
 fi
 }
+adg_renum=`nvram get adg_renum`
+
+adg_restart () {
+relock="/var/lock/AdGuardHome_restart.lock"
+if [ "$1" = "o" ] ; then
+	nvram set adg_renum="0"
+	[ -f $relock ] && rm -f $relock
+	return 0
+fi
+if [ "$1" = "x" ] ; then
+	adg_renum=${adg_renum:-"0"}
+	adg_renum=`expr $adg_renum + 1`
+	nvram set adg_renum="$adg_renum"
+	if [ "$adg_renum" -gt "3" ] ; then
+		I=19
+		echo $I > $relock
+		logger -t "【AdGuardHome】" "多次尝试启动失败，等待【"`cat $relock`"分钟】后自动尝试重新启动"
+		while [ $I -gt 0 ]; do
+			I=$(($I - 1))
+			echo $I > $relock
+			sleep 60
+			[ "$(nvram get adg_renum)" = "0" ] && break
+   			#[ "$(nvram get adg_enable)" = "0" ] && exit 0
+			[ $I -lt 0 ] && break
+		done
+		nvram set adg_renum="1"
+	fi
+	[ -f $relock ] && rm -f $relock
+fi
+scriptname=$(basename $0)
+if [ ! -z "$scriptname" ] ; then
+	eval $(ps -w | grep "$scriptname" | grep -v $$ | grep -v grep | awk '{print "kill "$1";";}')
+	eval $(ps -w | grep "$scriptname" | grep -v $$ | grep -v grep | awk '{print "kill -9 "$1";";}')
+fi
+start_adg
+}
 
 find_bin() {
+SVC_PATH="$(nvram get adg_bin)"
+
 dirs="/etc/storage/bin
 /tmp/AdGuardHome
 /usr/bin"
 
-SVC_PATH=""
-for dir in $dirs ; do
+if [ -z "$SVC_PATH" ] ; then
+  for dir in $dirs ; do
     if [ -f "$dir/AdGuardHome" ] ; then
         SVC_PATH="$dir/AdGuardHome"
         [ ! -x "$SVC_PATH" ] && chmod +x $SVC_PATH
         break
     fi
-done
-[ -z "$SVC_PATH" ] && SVC_PATH="/tmp/AdGuardHome/AdGuardHome"
+  done
+  [ -z "$SVC_PATH" ] && SVC_PATH="/tmp/AdGuardHome/AdGuardHome"
+fi
 }
 
 get_tag() {
@@ -121,28 +160,31 @@ dl_adg() {
 	if [ ! -f "$SVC_PATH" ] ; then
 		logger -t "【AdGuardHome】" "找不到 $SVC_PATH ，下载 AdGuardHome 程序"
 		get_tag
+  		adg_path=$(dirname "$SVC_PATH")
 		logger -t "【AdGuardHome】" "下载${tag}版本 下载较慢，耐心等待"
 		for proxy in $github_proxys ; do
+  			length=$(wget --no-check-certificate -T 5 -t 3 "${proxy}https://github.com/AdguardTeam/AdGuardHome/releases/download/${tag}/AdGuardHome_linux_mipsle_softfloat.tar.gz" -O /dev/null --spider --server-response 2>&1 | grep "[Cc]ontent-[Ll]ength" | grep -Eo '[0-9]+' | tail -n 1)
+ 			length=`expr $length + 512000`
+			length=`expr $length / 1048576`
+ 			adg_size0="$(check_disk_size $SVC_PATH)"
+ 			[ ! -z "$length" ] && logger -t "【AdGuardHome】" "程序大小 ${length}M， 程序路径可用空间 ${adg_size0}M "
 			curl -Lkso "/tmp/AdGuardHome/AdGuardHome.tar.gz" "${proxy}https://github.com/AdguardTeam/AdGuardHome/releases/download/${tag}/AdGuardHome_linux_mipsle_softfloat.tar.gz" || wget --no-check-certificate -q -O "/tmp/AdGuardHome/AdGuardHome.tar.gz" "${proxy}https://github.com/AdguardTeam/AdGuardHome/releases/download/${tag}/AdGuardHome_linux_mipsle_softfloat.tar.gz"
 			if [ "$?" = 0 ] ; then
-				tar -xzvf /tmp/AdGuardHome/AdGuardHome.tar.gz -C /tmp
-		 		cd /tmp/AdGuardHome ; rm -f ./AdGuardHome.tar.gz ./LICENSE.txt./README.md ./CHANGELOG.md ./AdGuardHome.sig
-		 		chmod +x /tmp/AdGuardHome/AdGuardHome
-				if [ $(($(/tmp/AdGuardHome/AdGuardHome -h | wc -l))) -gt 3 ] ; then
-					logger -t "【AdGuardHome】" "/tmp/AdGuardHome/AdGuardHome 下载成功"
+				tar -xzvf /tmp/AdGuardHome/AdGuardHome.tar.gz -C $adg_path
+    				rm -f /tmp/AdGuardHome/AdGuardHome.tar.gz
+		 		cd ${adg_path} ; rm -f ./LICENSE.txt./README.md ./CHANGELOG.md ./AdGuardHome.sig
+		 		chmod +x $SVC_PATH
+				if [[ "$($SVC_PATH -h 2>&1 | wc -l)" -gt 3 ]] ; then
+					logger -t "【AdGuardHome】" "$SVC_PATH 下载成功"
 					break
-       			else
+       				else
 	   				logger -t "【AdGuardHome】" "下载不完整"
-					rm -f /tmp/AdGuardHome/AdGuardHome
+					rm -f $SVC_PATH
 	  			fi
 	  		else
-	  			logger -t "【AdGuardHome】" "下载失败，请手动下载 ${proxy}https://github.com/AdguardTeam/AdGuardHome/releases/download/${tag}/AdGuardHome_linux_mipsle_softfloat.tar.gz 解压上传到  /tmp/AdGuardHome/AdGuardHome"
+	  			logger -t "【AdGuardHome】" "下载失败，请手动下载 ${proxy}https://github.com/AdguardTeam/AdGuardHome/releases/download/${tag}/AdGuardHome_linux_mipsle_softfloat.tar.gz 解压上传到 $SVC_PATH"
 		 	fi
 		done
-	fi      
-	if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
-		logger -t "【AdGuardHome】" "程序将安装在内存/tmp/AdGuardHome/AdGuardHome 将会占用部分内存，请注意内存使用容量！"
-		SVC_PATH="/tmp/AdGuardHome/AdGuardHome"
 	fi     
 }
 
@@ -164,7 +206,8 @@ start_adg() {
   logger -t "【AdGuardHome】" "正在启动..."
   sed -Ei '/【AdGuardHome】|^$/d' /tmp/script/_opt_script_check
   find_bin
-  if [ ! -f "$SVC_PATH" ] || [ $(($($SVC_PATH -h | wc -l))) -lt 3 ] ; then
+  [[ "$($SVC_PATH -h 2>&1 | wc -l)" -lt 3 ]] && rm $SVC_PATH
+  if [ ! -f "$SVC_PATH" ] || [[ "$($SVC_PATH -h 2>&1 | wc -l)" -lt 3 ]] ; then
   dl_adg
   fi
   adgenable=$(nvram get adg_enable)
@@ -176,6 +219,20 @@ start_adg() {
   [ ! -x "$SVC_PATH" ] && chmod +x $SVC_PATH
   eval "$SVC_PATH -c $adg_file -w /tmp/AdGuardHome -v" &
   adg_keep
+  sleep 4
+  	if [ ! -z "`pidof AdGuardHome`" ] ; then
+ 		mem=$(cat /proc/$(pidof AdGuardHome)/status | grep -w VmRSS | awk '{printf "%.1f MB", $2/1024}')
+   		cpui="$(top -b -n1 | grep -E "$(pidof AdGuardHome)" 2>/dev/null| grep -v grep | awk '{for (i=1;i<=NF;i++) {if ($i ~ /AdGuardHome/) break; else cpu=i}} END {print $cpu}')"
+		logger -t "【AdGuardHome】" "运行成功！"
+  		logger -t "【AdGuardHome】" "内存占用 ${mem} CPU占用 ${cpui}"
+  		adg_restart o
+		echo `date +%s` > /tmp/vntcli_time
+		vnt_rules
+	else
+		logger -t "【AdGuardHome】" "运行失败, 注意检查${VNTCLI}是否下载完整,10 秒后自动尝试重新启动"
+  		sleep 10
+  		adg_restart x
+	fi
   fi
 }
 
