0c42fc65328859e5f1eeb3fc04f1446672d17b41
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2025-01-17 15:50:53 +0800

    Update ttyd.sh

--- trunk/user/ttyd/ttyd.sh
+++ trunk/user/ttyd/ttyd.sh
@@ -2,12 +2,16 @@
 nvram_ttyd_port="$(nvram get ttyd_port)"
 [ -z "$nvram_ttyd_port" ] && nvram set ttyd_port=7681 && nvram commit
 port=${nvram_ttyd_port:-"7681"}
+ttyd_cmd="$(nvram get ttyd_cmd)"
+[ -z "$ttyd_cmd" ] && nvram set ttyd_cmd="-i br0 login" && nvram commit
 
 func_start(){
-	start-stop-daemon -S -b -x ttyd -- -i br0 -p "$port" login
+	logger -t "【TTYD】" "运行ttyd：start-stop-daemon -S -b -x ttyd -- -p ${port} ${ttyd_cmd}"
+	start-stop-daemon -S -b -x ttyd -- -p "$port" "$ttyd_cmd"
 }
 
 func_stop(){
+	logger -t "【TTYD】" "关闭ttyd"
 	killall -q ttyd
 }
 
